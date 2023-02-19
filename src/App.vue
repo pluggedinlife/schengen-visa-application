d2eecd3e87902d39f0014484f7fe166c874ba56e
Mikhail Nazaretyan <sgas5570@gmail.com>
2023-02-19 19:56:15 +0100

    Initialize Vue project

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <main-view />
+</template>
+
+<script>
+import MainView from "./components/MainView.vue";
+
+export default {
+  name: "App",
+  components: {
+    MainView,
+  },
+};
+</script>
